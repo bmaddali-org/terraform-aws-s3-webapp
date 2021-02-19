3014758e89b88f0b3b29079ac55699b6b604cb4c
bmaddali <bj.maddali@gmail.com>
2021-02-18 22:23:16 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,3 @@
 variable "region" {}
 variable "prefix" {}
 variable "name" {}
-variable "global_tags" {
-    type = "map"
-    default {
-        Environment = "Dev"
-        Name = "Team2"
-    }
-}
